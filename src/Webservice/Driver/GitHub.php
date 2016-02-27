1ce2cc7605f83e27da5bc77e549b0d9764a94899
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-02-27 12:06:25 +0100

    Add comments for auto completion

--- src/Webservice/Driver/GitHub.php
+++ src/Webservice/Driver/GitHub.php
@@ -5,6 +5,13 @@ namespace CvoTechnologies\GitHub\Webservice\Driver;
 use Cake\Network\Http\Client;
 use Muffin\Webservice\AbstractDriver;
 
+/**
+ * Class GitHub
+ *
+ * @method Client client() client(Client $client = null)
+ *
+ * @package CvoTechnologies\GitHub\Webservice\Driver
+ */
 class GitHub extends AbstractDriver
 {
 
